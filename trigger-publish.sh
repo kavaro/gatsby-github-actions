51bd4d11587af439b0bafbb8daf227c7ec5d72e2
karl <karl.van.rompaey@hotmail.com>
2019-11-11 09:25:36 +0000

    trigger-publish.sh

---
+++ trigger-publish.sh
@@ -0,0 +1,7 @@
+curl \
+  -u kavaro:$KAVARO_GITHUB_TOKEN \
+  -v POST \
+  -H "Accept: application/vnd.github.everest-preview+json" \
+  -H "Content-Type: application/json" \
+  --data '{"event_type": "build"}' \
+  "https://api.github.com/repos/kavaro/gatsby-github-actions/dispatches"
