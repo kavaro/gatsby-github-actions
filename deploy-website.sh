7b62b4fd63fb9c6528848b4041c9e51effde3bbd
karl <karl.van.rompaey@hotmail.com>
2019-11-11 16:07:42 +0000

    setup workflow deploy-website

--- deploy-website.sh
+++ deploy-website.sh
@@ -3,5 +3,5 @@ curl \
   -v POST \
   -H "Accept: application/vnd.github.everest-preview+json" \
   -H "Content-Type: application/json" \
-  --data '{"event_type": "build"}' \
+  --data '{"event_type": "deploy-website"}' \
   "https://api.github.com/repos/kavaro/gatsby-github-actions/dispatches"
